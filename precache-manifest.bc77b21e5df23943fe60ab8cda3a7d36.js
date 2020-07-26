90b4193e9861760d96998a619ea2daf35c62fd12
David Yang <didalfn88@gmail.com>
2020-07-26 20:17:09 +0900

    Updates

---
+++ precache-manifest.bc77b21e5df23943fe60ab8cda3a7d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6077d372455ba00b43ffcc97c8ef78",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "29417a845dc8291f674a",
+    "url": "/movie_app/static/css/main.b050ea0f.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29417a845dc8291f674a",
+    "url": "/movie_app/static/js/main.27af11cc.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
